38586dd3bbd196e4c22ff0a23ece8a40dbb93d06
Calvin <calvindunn147@gmail.com>
2023-09-20 09:46:08 +0200

    Added docstring to results_page/models.py

--- results_page/models.py
+++ results_page/models.py
@@ -1,6 +1,17 @@
 from django.db import models
 
 class CountryVote(models.Model):
+    """
+    Represents a country and the number of votes it has received.
+
+    Attributes:
+        country_name (str): The name of the country, limited to 100 characters.
+        votes (int): The number of votes received by the country, defaults to 0.
+
+    Methods:
+        __str__(): Returns a string representation of the country's name.
+    """
+    
     country_name = models.CharField(max_length=100)
     votes = models.IntegerField(default=0)
     
